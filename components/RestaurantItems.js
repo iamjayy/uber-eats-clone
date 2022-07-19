849a7a2e5c3de8ba15f5f0e2e3341c0b97d27086
iamjayy <jason_doan94@yahoo.com>
2022-07-19 03:29:13 -0400

    yelp API

--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -32,19 +32,34 @@ export const localRestaurants = [
   },
 ];
 
-export default function RestaurantItems() {
+export default function RestaurantItems({ navigation, ...props }) {
   return (
-    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
-      {localRestaurants.map((restaurant, index) => (
-        <View
+    <>
+      {props.restaurantData.map((restaurant, index) => (
+        <TouchableOpacity
           key={index}
-          style={{ marginTop: 1, padding: 15, backgroundColor: "white" }}
+          activeOpacity={1}
+          style={{ marginBottom: 30 }}
+          onPress={() =>
+            navigation.navigate("RestaurantDetail", {
+              name: restaurant.name,
+              image: restaurant.image_url,
+              price: restaurant.price,
+              reviews: restaurant.review_count,
+              rating: restaurant.rating,
+              categories: restaurant.categories,
+            })
+          }
         >
-          <RestaurantImage image={restaurant.image_url} />
-          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
-        </View>
+          <View
+            style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
+          >
+            <RestaurantImage image={restaurant.image_url} />
+            <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+          </View>
+        </TouchableOpacity>
       ))}
-    </TouchableOpacity>
+    </>
   );
 }
 
@@ -68,12 +83,12 @@ const RestaurantInfo = (props) => (
       flexDirection: "row",
       justifyContent: "space-between",
       alignItems: "center",
-      marginTop: 1,
+      marginTop: 10,
     }}
   >
     <View>
       <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
-      <Text style={{ fontSize: 13, color: "gray" }}>30-45 min</Text>
+      <Text style={{ fontSize: 13, color: "gray" }}>30-45 • min</Text>
     </View>
     <View
       style={{
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,37 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
-import RestaurantItems from "../components/RestaurantItems";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
+
+const YELP_API_KEY =
+  "NyNcf138_h6DWWN7YgLIR76pLAp4subrzUfNI4l0Kmdp67187vyLfVc5sANOtD4lbzIx_OicDDzre1bP3ceIbm3cDh6JTYoS4UPvmMwtCIR5xRMQqfOAnAm4-FfWYnYx";
 
 export default function Home() {
+  const [restaurantData, setRestaurantData] = React.useState([
+    localRestaurants,
+  ]);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Orlando";
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurantData(json.businesses));
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -14,7 +40,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
-        <RestaurantItems />
+        <RestaurantItems restaurantData={restaurantData} />
       </ScrollView>
     </SafeAreaView>
   );
