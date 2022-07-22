5b11debe8f893649550087eb0dd7c7c244a7d959
iamjayy <jason_doan94@yahoo.com>
2022-07-22 03:37:04 -0400

    fixed dependancies and added navigation.js

---
+++ navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function RootNavigation() {
+  const Stack = createStackNavigator();
+
+  const screenOptions = {
+    headerShown: false,
+  };
+  return <NavigationContainer />;
+}
