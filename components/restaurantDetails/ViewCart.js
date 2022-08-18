72da91eb715cc34ffa8f409095c9ce7666f4949a
iamjayy <jason_doan94@yahoo.com>
2022-08-18 02:24:07 -0400

    total USD add and remove to cart

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -1,8 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useSelector } from "react-redux";
 
 export default function ViewCart() {
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((prev, curr) => prev + curr, 0);
+
+  const totalUSD = total.toLocaleString("en", {
+    style: "currency",
+    currency: "USD",
+  });
+  console.log(totalUSD);
+
   return (
     <View
       style={{
--- redux/reducers/cartReducer.js
+++ redux/reducers/cartReducer.js
@@ -28,6 +28,8 @@ let cartReducer = (state = defaultState, action) => {
           restaurantName: action.payload.restaurantName,
         };
       }
+      console.log(newState, "👉");
+      return newState;
     }
 
     default:
