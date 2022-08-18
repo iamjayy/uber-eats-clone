554db08d9df495518f1bc083c97c529f13506c68
iamjayy <jason_doan94@yahoo.com>
2022-08-18 01:44:36 -0400

    added dispatch and onpress function connecting menuitemns add to cart

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import React from "react";
 import { Divider } from "react-native-elements";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { useDispatch } from "react-redux";
 
 const foods = [
   {
@@ -56,7 +57,16 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function MenuItems() {
+export default function MenuItems(restaurantName) {
+  const dispatch = useDispatch();
+
+  const selectItem = (item) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: { ...item, restaurantName: restaurantName },
+    });
+  };
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       {foods.map((food, index) => (
@@ -68,6 +78,7 @@ export default function MenuItems() {
                 borderRadius: 0,
               }}
               fillColor="green"
+              onPress={() => selectItem(food)}
             />
             <FoodInfo food={food} />
             <FoodImage food={food} />
