0bd768fe515b8a61110fce83f904835e80224fc1
iamjayy <jason_doan94@yahoo.com>
2022-08-22 01:42:53 -0400

    items checked into cart will retain if you leave

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import React from "react";
 import { Divider } from "react-native-elements";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const foods = [
   {
@@ -71,6 +71,13 @@ export default function MenuItems(restaurantName) {
     });
   };
 
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+  );
+
+  const isFoodInCart = (food, cartItems) =>
+    Boolean(cartItems.find((item) => item.title === food.title));
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       {foods.map((food, index) => (
@@ -83,6 +90,7 @@ export default function MenuItems(restaurantName) {
               }}
               fillColor="green"
               onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+              isChecked={isFoodInCart(food, cartItems)}
             />
             <FoodInfo food={food} />
             <FoodImage food={food} />
