7fd9fef028e85a4963e8035b5d2c5bddaa19e389
iamjayy <jason_doan94@yahoo.com>
2022-09-02 02:07:16 -0400

    fixed dependancies. view cart modal order items

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -1,11 +1,15 @@
-import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 export default function ViewCart() {
   const [modalVisible, setModalVisible] = useState(false);
 
-  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const { items, restaurantName } = useSelector(
+    (state) => state.cartReducer.selectedItems
+  );
+
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
     .reduce((prev, curr) => prev + curr, 0);
@@ -16,30 +20,53 @@ export default function ViewCart() {
   });
   console.log(totalUSD);
 
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: "flex-end",
+      backgroundColor: "rgba(0,0,0,0.7)",
+    },
+
+    modalCheckoutContainer: {
+      backgroundColor: "white",
+      padding: 16,
+      height: 500,
+      borderWidth: 1,
+    },
+
+    restaurantName: {
+      textAlign: "center",
+      fontWeight: "600",
+      fontSize: 18,
+      marginBottom: 10,
+    },
+
+    subtotalContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 15,
+    },
+
+    subtotalText: {
+      textAlign: "left",
+      fontWeight: "600",
+      fontSize: 15,
+      marginBottom: 10,
+    },
+  });
+
   const checkoutModalContent = () => {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 30,
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "black",
-            padding: 1,
-            borderRadius: 30,
-            width: 150,
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => setModalVisible(false)}>
-            <Text style={{ color: "white" }}>Checkout</Text>
-          </TouchableOpacity>
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalCheckoutContainer}>
+            <Text style={styles.restaurantName}>{restaurantName}</Text>
+            {items.map((item, index) => (
+              <OrderItem key={index} item={item} />
+            ))}
+          </View>
         </View>
-      </View>
+      </>
     );
   };
 
